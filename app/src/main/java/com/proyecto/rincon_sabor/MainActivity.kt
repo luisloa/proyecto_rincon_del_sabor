ef496b006db4eef54e94cdbbda112682cfbd3d69
Luis Enrique Jimenez Loa <kiqe7913@gmail.com>
2025-04-19 20:14:28 -0600

    commit general al proyecto rincon_del_sabor

---
+++ app/src/main/java/com/proyecto/rincon_sabor/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.proyecto.rincon_sabor
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.proyecto.rincon_sabor.ui.theme.Rincon_saborTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Rincon_saborTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Rincon_saborTheme {
+        Greeting("Android")
+    }
+}
